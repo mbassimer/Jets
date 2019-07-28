b22ac99accbc2b2c63fae8fd6d996ae444461325
mbassimer <mbassimer@gmail.com>
2019-07-28 15:01:00 -0600

    Working Code with all parameters.

--- src/com/skilldistillery/jet/Airfield.java
+++ src/com/skilldistillery/jet/Airfield.java
@@ -1,5 +1,5 @@
 package com.skilldistillery.jet;
-import java.util.Scanner;
+
 import java.io.*;
 import java.util.*;
 
@@ -9,6 +9,7 @@ public class Airfield {
 	List<Jet> ParkingLot;
 	String line;
 	private String Scanner;
+
 	public Airfield() {
 		ParkingLot = new ArrayList<Jet>();
 		lookupRecords(file);
@@ -33,13 +34,12 @@ public class Airfield {
 				else if (line.contains("Cargo")) {
 					CargoPlane j = new CargoPlane(type, model, speed, range, price);
 					ParkingLot.add(j);
-				}
-				else if (line.contains("Luxury")) {
-					LuxuryPlane j = new LuxuryPlane(type,model,speed, range,price);
+				} else if (line.contains("Luxury")) {
+					LuxuryPlane j = new LuxuryPlane(type, model, speed, range, price);
 					ParkingLot.add(j);
-					
-				}else {
-				 System.out.println("This vehicle is not a jet or luxury plane");
+
+				} else {
+					System.out.println("This vehicle is not a jet or luxury plane");
 				}
 			}
 
@@ -98,7 +98,7 @@ public class Airfield {
 
 	public void longestRange() {
 		double longestRange = 0;
-		
+
 		for (Jet jet : ParkingLot) {
 			if (jet.getRange() > longestRange) {
 				longestRange = jet.getRange();
@@ -106,81 +106,91 @@ public class Airfield {
 			}
 		}
 
-			for (Jet jet2 : ParkingLot) {
-				if (longestRange == jet2.getRange()) {
+		for (Jet jet2 : ParkingLot) {
+			if (longestRange == jet2.getRange()) {
 
-					System.out.println(jet2.toString() + " can fly the farthest!");
-				}
+				System.out.println(jet2.toString() + " can fly the farthest!");
+			}
+
+		}
+
+	}
+
+	public void loadAll() {
+		for (Jet jet : ParkingLot) {
+			if (jet instanceof CargoCarrier) {
+				System.out.println(jet.toString());
+				System.out.println("This plane is ready to be loaded....");
+				System.out.println("Please begin loading when ready. ");
+			}
+
+		}
+	}
+
+	public void fightAll() {
+		for (Jet jet : ParkingLot) {
+			if (jet instanceof CombatReady) {
+				System.out.println(jet.toString());
+				System.out.println("This plane is ready to fight");
+				System.out.println("Don't mess with Me!");
 
 			}
 
 		}
-	
-
-	public void loadAll() {
-		 for (Jet jet : ParkingLot) {
-			 if(jet instanceof CargoCarrier) {
-			 System.out.println(jet.toString());
-			 System.out.println("This plane is ready to be loaded....");
-			 System.out.println("Please begin loading when ready. ");
-		 }
-
-		 }
-	}
-	public void fightAll() {
-		for (Jet jet : ParkingLot) {
-			 if(jet instanceof CombatReady) {
-			 System.out.println(jet.toString());
-			 System.out.println("This plane is ready to fight");
-			 System.out.println("Don't mess with Me!");
-			 
-		 }
-
-		 }
 
 	}
 
 	public void addJet() {
-		
-		Scanner kb= new Scanner(System.in);
-		 
-			
-			System.out.println("Please enter a jet type from Fighter, Cargo, or Luxury: ");
-			String type = kb.next();
-			System.out.println("Enter a model: ");
-			String model = kb.next();
-			System.out.println("Enter in the max speed:" );
-			double speed = kb.nextDouble();
-			System.out.println("Enter a range :" );
-			int range = kb.nextInt();
-			System.out.println("How much is the plane worth?");
-			long price = kb.nextLong();
-			
-			if (type.contains("Fighter")) {
-				Jet j = new Fighter(type, model, speed, range, price);
-				ParkingLot.add(j);
-			}
 
-			else if (type.contains("Cargo")) {
-				CargoPlane j = new CargoPlane(type, model, speed, range, price);
-				ParkingLot.add(j);
-			}
-			else if (type.contains("Luxury")) {
-				LuxuryPlane j = new LuxuryPlane(type,model,speed, range,price);
-				ParkingLot.add(j);
-				
-			}else {
-			 System.out.println("This vehicle is not a jet or luxury plane");
-			}
+		Scanner kb = new Scanner(System.in);
+
+		System.out.println("Please enter a jet type from Fighter, Cargo, or Luxury: ");
+		String type = kb.next();
+		System.out.println("Enter a model: ");
+		String model = kb.next();
+		System.out.println("Enter in the max speed:");
+		double speed = kb.nextDouble();
+		System.out.println("Enter a range :");
+		int range = kb.nextInt();
+		System.out.println("How much is the plane worth?");
+		long price = kb.nextLong();
+
+		if (type.contains("Fighter")) {
+			Jet j = new Fighter(type, model, speed, range, price);
+			ParkingLot.add(j);
 		}
-		
 
-	
+		else if (type.contains("Cargo")) {
+			CargoPlane j = new CargoPlane(type, model, speed, range, price);
+			ParkingLot.add(j);
+		} else if (type.contains("Luxury")) {
+			LuxuryPlane j = new LuxuryPlane(type, model, speed, range, price);
+			ParkingLot.add(j);
+
+		} else {
+			System.out.println("This vehicle is not a jet or luxury plane");
+		}
+	}
 
 	public void removeJet() {
+		Scanner kb = new Scanner(System.in);
 
-		
-		
+		{
+
+			for (int i = 0; i < ParkingLot.size(); i++) {
+				System.out.println((i + 1) + " " + ParkingLot.get(i).getModel());
+			}
+			int choice = kb.nextInt();
+			
+			ParkingLot.remove(choice - 1);
+			System.out.println("New inventory: ");
+			for (Jet jet : ParkingLot) {
+
+				System.out.println(jet);
+			}
+
+		}
+
 	}
 
 }
--- src/com/skilldistillery/jet/CargoCarrier.java
+++ src/com/skilldistillery/jet/CargoCarrier.java
@@ -4,8 +4,6 @@ public interface CargoCarrier  {
 	
 	public static void Loading() {
 		
-		System.out.println("This plane is ready to be loaded....");
-		System.out.println("Please begin loading when ready. ");
 		
 	}
 
